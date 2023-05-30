02559ffbe6d1904c11fcf48b31dbfce8019e1c87
Erick Simões <erick.simoes@live.com>
2023-05-30 15:10:29 -0300

    Climate Class (#9)
    * feat: Add a file to map all API weather options
    
    * wip: Initial implementation of the weather class
    
    * fix: Remove index column from csv climate_map file
    
    * fix: Reorder climate registers in climate_map.csv
    
    * feat: Add str magic method to Climate class
    
    * refactor: Weather unit becomes an inner class
    
    A container, such as a list, now has a weather unit. It will be able to
    return the reference object by its two different keys and calculate its
    distance compared to another weather unit.
    
    * feat: Implements getitem for climate object
    
    Use the __getitem__ magic method to return the item if the key
    exists in the list of icon_numbers
    
    * feat: Raise IndexError when climate is not found
    
    * refactor: Use enumerate intead of index count
    
    * feat: Add sub magic method in ClimateUnit
    
    The __sub__ magic method is used to calculate de diference
    between two Climate Units.
    
    * style: Add double space between methods
    
    * feat: Create climate instance in Climate module

---
+++ Carinha/emotional_rating/climate.py
@@ -0,0 +1,52 @@
+"""Describes the Azure API weather units"""
+
+import csv
+
+
+class Climate():
+    """The weather unit contanier"""
+
+    class ClimateUnit():
+        """The weather unit with its order identifier and description"""
+        def __init__(self, index, description, *icon_numbers):
+            self.index = index
+            self.description = description
+            self.icon_numbers = icon_numbers
+
+        def __str__(self):
+            return f'{self.index}\t{self.icon_numbers}    \t{self.description}'
+
+        def __sub__(self, other):
+            return abs(self.index - other.index)
+
+    def __init__(self, lang='en'):
+        self.climates = self.__create_climates(lang)
+
+    def __getitem__(self, key):
+        for climate_unity in self.climates:
+            if key in climate_unity.icon_numbers:
+                return climate_unity
+
+        raise IndexError('climate index out of range')
+
+    def __create_climates(self, lang):
+        climate_units = []
+
+        with open(r'Carinha\emotional_rating\climate_map.csv', encoding='UTF-8') as climate_file:
+            climate_reader = csv.DictReader(climate_file)
+
+            text_day = 'lang text day'.replace('lang', lang)
+            #text_night = 'lang text night'.replace('lang', lang)
+
+            for index, climate_unity in enumerate(climate_reader):
+                icons = [int(climate_unity['icon number day'])]
+                if climate_unity['icon number night'] != '':
+                    icons.append(int(climate_unity['icon number night']))
+
+                climate_unit = self.ClimateUnit(index, climate_unity[text_day], *icons)
+                climate_units.append(climate_unit)
+
+        return climate_units
+
+
+climate = Climate('pt')
