ebcf81d0b00f20f1baf1441095069d28e72bdb2b
Erick Simões <erick.simoes@live.com>
2023-06-21 10:40:24 -0300

    doc: Add docstring in main file

--- Carinha/main.py
+++ Carinha/main.py
@@ -1,18 +1,24 @@
+"""CARINHA
+
+Emotive robotics artifact capable of expressing emotions based on climate and environmental data.
+
+This is a project by GARAGino, Research Group on Physical Computing at CESAR School
+"""
+
 from emotional_rating.weather_api import weather_api
 from emotional_rating.emotion import emotions
 from utils import millis
 
 tempoAnterior = millis()
 weather_api.get_weather()
+print(emotions.get_current_emotion())
 
 try:
     while True:
         if millis() - tempoAnterior >= 2760000:  # 46 minutos
             tempoAnterior = millis()
             weather_api.get_weather()
-            current_emotion = emotions.get_current_emotion()
-            print(current_emotion)
-
+            print(emotions.get_current_emotion())
 
 except KeyboardInterrupt:
     print('C.A.R.I.N.H.A. finalizado')
