777e945f730669651f301f69c3b205771f6abb6b
Erick Simões <erick.simoes@live.com>
2023-06-21 10:22:27 -0300

    style: Reduce public variables from weather_api
    Remove excess comments and add add timeout in request method.

--- Carinha/emotional_rating/weather_api.py
+++ Carinha/emotional_rating/weather_api.py
@@ -9,14 +9,14 @@ class WeatherApi():
     def get_weather(self):
         """Queries the weather API and saves relevant data to a JSON file"""
 
-        token = '5pU_kiVDbnaTRqp8feiC0GktoGJ5iOwV0-cPYZ-KZhs' # Define a chave de acesso da API
-        coord = '-8.059641978650072,-34.87248963042983' # Define a coordenada que vamos usar
-        unit = 'metric'  # Define a unidade de medida dos dados. metric = celcius/km
-        details = 'true' # Da mais detalhes sobre o clima, se for falso só retorna o clima predominante
-        duration = 0  # 0 = atual, 6 = ultimas 6h, 24 = ultimas 24h
-        language = 'pt-BR'  # idioma do clima predominante
+        token = '5pU_kiVDbnaTRqp8feiC0GktoGJ5iOwV0-cPYZ-KZhs'
+        coord = '-8.059641978650072,-34.87248963042983' #CESAR School from Recife
+        unit = 'metric' #metric = celcius/km
+        details = 'true'
+        duration = 0 #0 = current, 6 = last 6h, 24 = last 24h
+        language = 'pt-BR'
 
-        # Link de chamada da API
+        #Link de chamada da API
         api = f'https://atlas.microsoft.com/weather/currentConditions/json?api-version=1.1'\
             f'&query={coord}'\
             f'&unit={unit}'\
@@ -26,29 +26,17 @@ class WeatherApi():
             f'&subscription-key={token}'
 
         # Usa a biblioteca 'requests' para pegar os dados da API e transforma num arquivo json
-        dados = requests.get(url=api).json()
-
-        # dados da API que vamos utilizar:
-        weather = dados['results'][0]['phrase']  # A "frase" do clima atual
-        # Codigo do icone, pode nos ser útil
-        icon_code = dados['results'][0]['iconCode']
-        # Retorna true se estiver de dia
-        is_day_time = dados['results'][0]['isDayTime']
-        # A temperatura em Celcius
-        temp = dados['results'][0]['temperature']['value']
-        # A veliodcidade do vento em km/h
-        wind_speed = dados['results'][0]['wind']['speed']['value']
-        date_time = dados['results'][0]['dateTime']
+        dados = requests.get(url=api, timeout=30).json()['results'][0]
 
         # Dicionario com todos os dados
         file = {
-            'dateTime': date_time,
+            'dateTime': dados['dateTime'],
             'weekDay': week_day_list[date.weekday(date.today())],
-            'weather': weather,
-            'iconCode': icon_code,
-            'isDayTime': is_day_time,
-            'temperature': temp,
-            'windSpeed': wind_speed
+            'weather': dados['phrase'],
+            'iconCode': dados['iconCode'],
+            'isDayTime': dados['isDayTime'],
+            'temperature': dados['temperature']['value'],
+            'windSpeed': dados['wind']['speed']['value']
         }
 
         # Função que transforma dicionario em arquivo json
