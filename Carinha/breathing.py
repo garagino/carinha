36e25816c0c2919ca1f7ea10f77bc51facebc069
Erick Simões <erick.simoes@live.com>
2023-06-25 19:53:02 -0300

    refactor: Predefined sequence of numbers
    Defines sequence of numbers of the luminous intensity of the breath.
    Now only a for loop is needed.

--- Carinha/breathing.py
+++ Carinha/breathing.py
@@ -8,17 +8,14 @@ class Breathing(Thread):
         Thread.__init__(self)
         self.frenquency = frequency
         self.led_pin = led_pin
+        self.sequence = list(range(0, 100)) + list(range(100, 0, -1))
 
         GPIO.setup(self.led_pin, GPIO.OUT)
         self.pwm = GPIO.PWM(self.led_pin, 100)
         self.pwm.start(0)
 
     def breathe(self):
-        for intensity in range(0, 101):
-            self.pwm.ChangeDutyCycle(intensity)
-            sleep(self.frenquency)
-
-        for intensity in range(100, -1, -1):
+        for intensity in self.sequence:
             self.pwm.ChangeDutyCycle(intensity)
             sleep(self.frenquency)
 
