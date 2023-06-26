9c93f85f3ba9f47b92a5dc4511bc96a69126aa85
Erick Simões <erick.simoes@live.com>
2023-06-26 13:53:15 -0300

    refactor: Remove try loop block from main

--- Carinha/main.py
+++ Carinha/main.py
@@ -7,21 +7,7 @@ This is a project by GARAGino, Research Group on Physical Computing at CESAR Sch
 
 from emotional_rating.weather_api import weather_api
 from emotional_rating.emotion import emotions
-from utils import millis
 
-tempoAnterior = millis()
+
 weather_api.start()
 print(emotions.get_current_emotion())
-
-try:
-    while True:
-        if millis() - tempoAnterior >= 2760000:  # 46 minutos
-            tempoAnterior = millis()
-            weather_api.get_weather()
-            print(emotions.get_current_emotion())
-
-except KeyboardInterrupt:
-    print('C.A.R.I.N.H.A. finalizado')
-
-finally:
-    pass
