f0e57f96b55ae7b58534385b007f1ce0e578365d
Erick Simões <erick.simoes@live.com>
2023-07-11 00:22:15 -0300

    feat: New strategy to count interaction time
    Now, the number of interactions and their duration are counted.

--- Carinha/presence_sensor.py
+++ Carinha/presence_sensor.py
@@ -1,26 +1,27 @@
+from datetime import datetime
 from threading import Thread
-from time import sleep
-import RPi.GPIO as GPIO  
+from gpiozero import MotionSensor
 
 class PresenceSensor(Thread):
     def __init__(self, pir_sensor):
         Thread.__init__(self)
-        self.pir_sensor = pir_sensor
-        self.presence = False
 
-        GPIO.setup(self.pir_sensor, GPIO.IN)
+        self.pir_sensor = MotionSensor(pir_sensor)
 
     def sense(self):
-        timer = 0
         while True:
-            if GPIO.input(self.pir_sensor):
-                timer += 1
-                sleep(1)
-            else:
-                timer = 0
-            
-            if timer >= 3:
-                self.presence = True
+            self.pir_sensor.wait_for_motion()
+            initial_time = datetime.now()
+            #TODO: log initial interaction
+            #TODO: activate presence
+
+            self.pir_sensor.wait_for_no_motion()
+            delta_time = datetime.now() - initial_time
+            self.__log_time(initial_time, delta_time)
+
+    def __log_time(self, initial_interaction, duration):
+        #TODO: Save initial interacion time and duration in csv file
+        pass
 
     def run(self):
         self.sense()
