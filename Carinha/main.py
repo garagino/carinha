f2c6ff0d0ae5224b7c3ba135be291c978bf53ee4
Erick Simões <erick.simoes@live.com>
2023-07-15 18:14:59 -0300

    feat: Add scope of execution check

--- Carinha/main.py
+++ Carinha/main.py
@@ -10,6 +10,13 @@ from emotional_rating.emotion import emotions
 from breathing import breathe
 
 
-weather_api.start()
-breathe.start()
-print(emotions.get_current_emotion())
+def main():
+    """Starts CARINHA threads"""
+
+    weather_api.start()
+    breathe.start()
+
+    print(emotions.get_current_emotion())
+
+if __name__ == '__main__':
+    main()
